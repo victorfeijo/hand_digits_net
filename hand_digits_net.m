33902699dcc845a00064e19575282615a7d36673
Victor Feijo <victor.feijoa@gmail.com>
2016-11-27 18:06:07 -0200

    adds right display data

--- hand_digits_net.m
+++ hand_digits_net.m
@@ -2,9 +2,16 @@ pkg load nnet;
 
 load('ex4data1.mat')
 
-% Invert data set to fit rows and not columns
-%X = X';
-%y = y';
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
+
+displayData(sel);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 % hidden neurons, output neurons
 hidden_neurons = 16;
